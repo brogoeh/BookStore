bb3881fe00812f9ddd3ea3a99995828d40b1914a
brogoeh <tgoeh03@gmail.com>
2020-02-19 13:20:12 +0700

    Change data

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended : false, limit: '10mb'}))
 
 const mongoose      = require('mongoose')
 
-mongoose.connect('mongodb://localhost/bookStore',{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/bookStore',{ useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose'))
